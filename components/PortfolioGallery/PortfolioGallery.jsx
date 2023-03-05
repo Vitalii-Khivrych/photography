d54bffddbaf8c057694e9e9292542955b9525e62
Vitalii Khivrych <khivr.vitali@gmail.com>
2023-03-05 15:43:39 +0200

    refactoring render portfolio gallery

--- components/PortfolioGallery/PortfolioGallery.jsx
+++ components/PortfolioGallery/PortfolioGallery.jsx
@@ -2,38 +2,27 @@ import Image from 'next/image';
 import 'lightbox.js-react/dist/index.css';
 import { SlideshowLightbox } from 'lightbox.js-react';
 
+import { data } from '@/data/galleries';
 import { List, Item } from './PortfolioGallery.styled';
 
 export const PortfolioGallery = ({ id }) => {
-  const data = [
-    { id: 1, href: `/images/galleries/${id}/0.jpg` },
-    { id: 2, href: `/images/galleries/${id}/1.jpg` },
-    { id: 3, href: `/images/galleries/${id}/2.jpg` },
-    { id: 4, href: `/images/galleries/${id}/3.jpg` },
-    { id: 5, href: `/images/galleries/${id}/4.jpg` },
-    { id: 6, href: `/images/galleries/${id}/5.jpg` },
-    { id: 7, href: `/images/galleries/${id}/6.jpg` },
-    { id: 8, href: `/images/galleries/${id}/7.jpg` },
-    { id: 9, href: `/images/galleries/${id}/8.jpg` },
-    { id: 10, href: `/images/galleries/${id}/9.jpg` },
-    { id: 11, href: `/images/galleries/${id}/10.jpg` },
-    { id: 12, href: `/images/galleries/${id}/11.jpg` },
-  ];
+  const [filterData] = data.filter(item => item.id === id);
+  const imagesArr = filterData.data;
 
   return (
     <SlideshowLightbox
       lightboxIdentifier="lightbox1"
       framework="next"
-      data={data}
+      data={imagesArr}
     >
       <List>
-        {data.map(({ id, href }) => (
-          <Item key={id}>
+        {imagesArr.map(({ href }, ind) => (
+          <Item key={ind}>
             <Image
               src={href}
               alt={href}
-              height={500}
-              width={500}
+              height={200}
+              width={200}
               data-lightboxjs="lightbox1"
               quality={100}
               style={{
--- next.config.js
+++ next.config.js
@@ -10,12 +10,9 @@ const nextConfig = {
   images: {
     disableStaticImages: true,
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
-
-// module.exports = nextConfig;
 
 module.exports = withImages({
   webpack(config, options) {
